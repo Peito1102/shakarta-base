d6b1f278bfbe454c3c1a4bf06b29b26d1fe18358
Peito1102 <renzitiwin@gmail.com>
2025-02-26 22:48:52 -0500

    pequeños cambios

--- src/main/java/org/aguzman/apiservlet/webapp/headers/util/ConexionBaseDatos.java
+++ src/main/java/org/aguzman/apiservlet/webapp/headers/util/ConexionBaseDatos.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConexionBaseDatos {
 
-    private static String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=America/Santiago";
+    private static String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=America/Lima";
     private static String username = "root";
-    private static String password = "sasa";
+    private static String password = "mysql";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, username, password);
